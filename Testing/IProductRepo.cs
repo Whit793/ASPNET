60e577573687cf3901846f8fbbc6efa24f85cdde
Bryan Whitman <BW.Hitman89@gmail.com>
2020-12-26 10:05:52 -0600

    updated product tab on website

---
+++ Testing/IProductRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<ProductModel> GetAllProducts();
+        public ProductModel GetProduct(int id);
+    }
+}
---
+++ Testing/ProductRepo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<ProductModel> GetAllProducts()
+        {
+            return _conn.Query<ProductModel>("SELECT * FROM products;");
+        }
+
+        public ProductModel GetProduct(int id)
+        {
+            return (ProductModel)_conn.QuerySingle<ProductModel>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
+        }
+
+        
+        
+    }
+}
